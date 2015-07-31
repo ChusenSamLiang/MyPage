f7dca7460e19546f80881b53f2342d3cc6fdcc37
Chusen Sam Liang <liang.chusen@gmail.com>
2015-07-31 15:59:06 -0400

    social media

--- js/profile.js
+++ js/profile.js
@@ -1,65 +1,174 @@
 
+var action = "show";
 
-// jQuery to transition profile about me blocks
+$("#profile .details .topic .main").on("click", viewProfile);
+
+function viewProfile() {
+    if(action == "show") {
+        $("#profile .details .topic .main").css("background-color","red");
+        $("#profile .details .topic .main").css("left","0px");
+        $('#profile .details .subtopic').show();
+        $('#profile .details .subtopic').css("width","55%");
+        $('#profile .details .topic').css("width","45%");
+        action = "hide";
+    }
+    else{
+        $("#profile .details .topic .main").css("background-color","yellow");
+        $("#profile .details .topic .main").css("left","100px");
+        $('#profile .details .subtopic').hide();
+        action = "show";
+    }
+}
+
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
 
 var action = "show";
 
 $("#profile .details .topic .main").on("click", viewProfile);
 
 function viewProfile() {
-	if(action == "show") {
-		$("#profile .details .topic .main").css("background-color","red");
-		$("#profile .details .topic .main").css("left","0px");
-		$('#profile .details .subtopic').show();
-		$('#profile .details .subtopic').css("width","55%");
-		$('#profile .details .topic').css("width","45%");
-		action = "hide";
-	}
-	else{
-		$("#profile .details .topic .main").css("background-color","yellow");
-		$("#profile .details .topic .main").css("left","100px");
-		$('#profile .details .subtopic').hide();
-		action = "show";
-	}
+    if(action == "show") {
+        $("#profile .details .topic .main").css("background-color","red");
+        $("#profile .details .topic .main").css("left","0px");
+        $('#profile .details .subtopic').show();
+        $('#profile .details .subtopic').css("width","55%");
+        $('#profile .details .topic').css("width","45%");
+        action = "hide";
+    }
+    else{
+        $("#profile .details .topic .main").css("background-color","yellow");
+        $("#profile .details .topic .main").css("left","100px");
+        $('#profile .details .subtopic').hide();
+        action = "show";
+    }
 }
 
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
 
 
-  //   $("#profile .details .topic .main").click(function(){
-	 //    	$("#profile .details .topic .main").css("background-color","red");
-		// 	$("#profile .details .topic .main").css("left","0%");
-	 //    	$('#profile .details .subtopic').show();
-	 //    	$('#profile .details .subtopic').css("background-color","yellow");
-		// });
-	 // //    .mouseout(function(){
-		// // 	$("#profile .details .topic .main").css("background-color","yellow");
-		// // 	$("#profile .details .topic .main").css("left","25%");
-		// // 	$('#profile .details .subtopic').hide();
-		// // });
-    	
+/*
+Circle
+*/
 
-	// // $('#profile .details .middle').stop().animate({'margin-top': '200px'}, 1500);
-	// //     $('#profile .details .top').stop().animate({'margin-bottom': '-200px'}, 1500);
-	//  //    , 
-	//  //    function(){
-	//  //        $('#profile .details .top').stop().animate({'margin-bottom': '50px'}, 1500);
-	// 	// }
+// var pos = $('#contact').position(),
+//     radiusSat = $('.media1').width() * 0.5,
+//     radius = $('#contact').width() * 0.5,
+//     cx = pos.left + radius,
+//     cy = pos.top + radius,
+//     x, y, angle = 0, angles = [],
+//     spc = 360 / 5,
+//     deg2rad = Math.PI / 180,
+//     i = 0;
 
+// for(;i < 5; i++) {
+//     angles.push(angle);
+//     angle += spc;
+// }
 
-	// 	 $('#profile .details .bottom').stop().animate({'margin-top': '400px'}, 1500);
-	//  //    , 
-	//  //    function(){
-	//  //        $('#profile .details .bottom').stop().animate({'margin-top': '50px'}, 1500);
-	// 	// }
+// /// space out radius
+// radius += (radiusSat + 10);
 
-	// 	 $('#profile .details .left').stop().animate({'margin-right': '300px'}, 1500);
-	//  //    , 
-	//  //    function(){
-	//  //        $('#profile .details .left').stop().animate({'margin-right': '50px'}, 1500);
-	// 	// }
+// loop();
 
-	// 	 $('#profile .details .right').stop().animate({'margin-left': '-300px'}, 1500);
-	    
-	  
+// function loop() {
 
+//     for(var i = 0; i < angles.length; i++) {
 
+//         angle = angles[i];
+        
+//         x = cx + radius * Math.cos(angle * deg2rad);
+//         y = cy + radius * Math.sin(angle * deg2rad);
+
+//         $('media' + i).css({left:x - radiusSat, top:y - radiusSat});
+    
+//         angles[i] = angles[i] + 1;
+//         if (angles[i] > 360) angles[i] = 0;
+//     }
+    
+//     requestAnimationFrame(loop);
+// }
+
+var pos = $('#contact .center').position(),
+    radiusSat = $('#contact .sat1').width() * 0.5,
+    radius = $('#contact .center').width() * 0.5,
+    cx = pos.left + radius,
+    cy = pos.top + radius,
+    x, y, angle = 0, angles = [],
+    spc = 360 / 5,
+    deg2rad = Math.PI / 180,
+    i = 0;
+
+for(;i < 5; i++) {
+    angles.push(angle);
+    angle += spc;
+}
+
+/// space out radius
+radius += (radiusSat + 10);
+
+loop();
+
+function loop() {
+
+    for(var i = 0; i < angles.length; i++) {
+
+        angle = angles[i];
+        
+        x = cx + radius * Math.cos(angle * deg2rad);
+        y = cy + radius * Math.sin(angle * deg2rad);
+
+        $('#contact .sat' + i).css({left:x - radiusSat, top:y - radiusSat});
+    
+        angles[i] = angles[i] + 1;
+        if (angles[i] > 360) angles[i] = 0;
+    }
+    
+    requestAnimationFrame(loop);
+}
